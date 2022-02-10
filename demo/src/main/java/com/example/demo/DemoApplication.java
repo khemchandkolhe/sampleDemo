027212ababfd35ae689b709ac06906e13f63b810
dev <dev@gmail.com>
2022-02-10 16:02:01 +0530

    date clad deleted by tl

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,7 @@
 package com.example.demo;
 
-import java.util.Date;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.example.demo.Date.DateDemo;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -14,8 +10,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 		
 		
-		Date date = DateDemo.getDate();
-		System.out.println(date);
+		
 	}
 
 }
