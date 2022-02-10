97bb66527c008c3494ebc8dc50cae2abc1edd7e0
dev <dev@gmail.com>
2022-02-10 15:32:49 +0530

    date util added by tl

---
+++ demo/src/main/java/com/example/demo/Date/DateDemo.java
@@ -0,0 +1,12 @@
+package com.example.demo.Date;
+
+import java.util.Date;
+
+public class DateDemo {
+
+	
+
+	public static Date getDate() {
+		return new Date();
+	}
+}
--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.Date.DateDemo;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		
+		
+		Date date = DateDemo.getDate();
+		System.out.println(date);
 	}
 
 }
